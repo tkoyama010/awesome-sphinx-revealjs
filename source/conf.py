916ad9cc3d7a806a70d8860e6c31559e382dd66d
Tetsuo Koyama <tkoyama010@gmail.com>
2026-01-31 14:36:11 +0900

    Add sphinx-revealjs with stlite integration, GitHub Pages deployment, and PR preview

---
+++ source/conf.py
@@ -0,0 +1,19 @@
+project = 'Awesome sphinx-revealjs'
+copyright = '2026, Tetsuo Koyama'
+author = 'Tetsuo Koyama'
+
+extensions = [
+    'sphinx_revealjs',
+]
+
+html_theme = 'sphinx_revealjs'
+
+revealjs_static_path = ['_static']
+revealjs_style_theme = 'black'
+revealjs_script_conf = {
+    'controls': True,
+    'progress': True,
+    'history': True,
+    'center': True,
+    'transition': 'slide',
+}
