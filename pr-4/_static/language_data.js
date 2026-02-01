4ea431ac87b0ebc40650d4e563e5560f179f7169
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-02-01 03:45:27 +0000

    deploy: 351cb5f395a16dd9992564127d663bd95859144b

--- pr-4/_static/language_data.js
+++
@@ -1,8 +0,0 @@
-/*
- * This script contains the language-specific data used by searchtools.js,
- * namely the set of stopwords, stemmer, scorer and splitter.
- */
-
-const stopwords = new Set();
-window.stopwords = stopwords;  // Export to global scope
-
